911560d1b831f148ac24e3f941118686551d62ac
alou1994 <40177649+cams1990@users.noreply.github.com>
2024-11-30 23:29:07 -0600

    fix/ASSIGN_Lab2 (#2)

--- src/lab2/Address.java
+++ src/lab2/Address.java
@@ -1,11 +1,11 @@
-package lessontwo;
+package lab2;
 
-public class Adress {
+public class Address {
     private String street;
     private String city;
     private String zip;
 
-    public Adress(String street, String city, String zip) {
+    public Address(String street, String city, String zip) {
         this.street = street;
         this.city = city;
         this.zip = zip;
