46ec2e35dc00fe134741048c62e5bf78b856dc24
Pabin R Luitel <pabin2050@gmail.com>
2024-12-17 17:40:12 -0600

    add assignment for lesson 12 prob1 and prob2

---
+++ src/lesson12/problem2/BalanceLessThanHundredException.java
@@ -0,0 +1,11 @@
+package lesson12.problem2;
+
+public class BalanceLessThanHundredException extends Exception {
+    BalanceLessThanHundredException(String message) {
+        super(message);
+    }
+
+    BalanceLessThanHundredException() {
+        super();
+    }
+}
